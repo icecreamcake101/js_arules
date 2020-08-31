bddb1b676d2112b48c0ab5efea4d2853b86df53b
bacloud14 <bacloud14@gmail.com>
2020-08-31 20:23:22 +0200

    wip, added d3 graph

--- app.js
+++ app.js
@@ -261,11 +261,6 @@
                 return isSubSetArray;
             };
             ArrayUtils.getDiffArray = function(arrayA, arrayB) {
-				/*
-					arrayA = new Set(arrayA)
-					arrayB = new Set(arrayB)
-					return [...new Set([...arrayA].filter(x => !arrayB.has(x)))]
-				*/
                 var diffArray = [];
                 arrayA.forEach(function(e) {
                     if (arrayB.indexOf(e) === -1)
@@ -749,6 +744,17 @@ function handleFileSelect(evt) {
 						'value': Math.floor(elem.confidence * 10 + 1)
 					}
 				})
+			links2 = result0
+				.filter(function (elem) {
+					return elem.confidence < 1
+				})
+				.map(function (elem) {
+					return {
+						'source': elem['rhs'][0],
+						'target': elem['lhs'][0],
+						'value': Math.floor(elem.confidence * 10 + 1)
+					}
+				})
 			nodes = _.map(nodes, function (node) {
 				return {
 					name: node,
@@ -760,8 +766,13 @@ function handleFileSelect(evt) {
 				nodes: nodes,
 				links: links
 			}
+			graph = {
+				nodes: nodes,
+				links: links2
+			}			
+			
 			createAdjacencyMatrix(input);
-			document.getElementById("adjacency-matrix-layout").setAttribute("style", "overflow:auto; height:960px; width:960px;")
+			document.getElementById("adjacency").setAttribute("style", "overflow:auto; height:960px; width:960px;")
 			function createAdjacencyMatrix(data) {
 				const adjacencyMatrix = d3.adjacencyMatrixLayout();
 				console.log('adjacencyMatrix', adjacencyMatrix);
@@ -805,6 +816,95 @@ function handleFileSelect(evt) {
 				d3.select('#adjacencyG')
 					.call(adjacencyMatrix.yAxis);
 			}
+			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			
+			createGraph(graph)
+			function createGraph(graph) {
+				var svg = d3.select("#graph"),
+					width = +svg.attr("width"),
+					height = +svg.attr("height");
+
+				var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+				var simulation = d3.forceSimulation()
+					.force("link", d3.forceLink().id(function(d) { return d.name; }))
+					.force("charge", d3.forceManyBody())
+					.force("center", d3.forceCenter(width / 2, height / 2));
+
+
+
+				var link = svg.append("g")
+				  .attr("class", "links")
+				.selectAll("line")
+				.data(graph.links)
+				.enter().append("line")
+				  .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+				var node = svg.append("g")
+				  .attr("class", "nodes")
+				.selectAll("g")
+				.data(graph.nodes)
+				.enter().append("g")
+
+				var circles = node.append("circle")
+				  .attr("r", 5)
+				  .attr("fill", function(d) { return color(d.group); })
+				  .call(d3.drag()
+					  .on("start", dragstarted)
+					  .on("drag", dragged)
+					  .on("end", dragended));
+
+				var lables = node.append("text")
+				  .text(function(d) {
+					return d.name;
+				  })
+				  .attr('x', 6)
+				  .attr('y', 3);
+
+				node.append("title")
+				  .text(function(d) { return d.name; });
+
+				simulation
+				  .nodes(graph.nodes)
+				  .on("tick", ticked);
+
+				simulation.force("link")
+				  .links(graph.links);
+
+				function ticked() {
+				link
+					.attr("x1", function(d) { return d.source.x; })
+					.attr("y1", function(d) { return d.source.y; })
+					.attr("x2", function(d) { return d.target.x; })
+					.attr("y2", function(d) { return d.target.y; });
+
+				node
+					.attr("transform", function(d) {
+					  return "translate(" + d.x + "," + d.y + ")";
+					})
+				}
+
+				function dragstarted(d) {
+				  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+				  d.fx = d.x;
+				  d.fy = d.y;
+				}
+
+				function dragged(d) {
+				  d.fx = d3.event.x;
+				  d.fy = d3.event.y;
+				}
+
+				function dragended(d) {
+				  if (!d3.event.active) simulation.alphaTarget(0);
+				  d.fx = null;
+				  d.fy = null;
+				}
+			}
+			document.getElementById("graph").setAttribute("style", "overflow:auto; height:960px; width:960px;")
+			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			
+			
 		}
 	});
 }
